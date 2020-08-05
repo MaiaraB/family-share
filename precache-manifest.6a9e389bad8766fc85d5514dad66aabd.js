b5a181415e9a24a0b02a811a16b003d0f6ddc1a1
Maiara Barroso <maiarabarroso84@gmail.com>
2020-08-05 19:11:51 +0100

    Updates

---
+++ precache-manifest.6a9e389bad8766fc85d5514dad66aabd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689a01fe1b01f18a91a741fc86c40384",
+    "url": "/family-share/index.html"
+  },
+  {
+    "revision": "f4d0722831fa8064718f",
+    "url": "/family-share/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "fa888cef653cd6654ad8",
+    "url": "/family-share/static/js/2.8c1a22f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/family-share/static/js/2.8c1a22f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d0722831fa8064718f",
+    "url": "/family-share/static/js/main.a73ff326.chunk.js"
+  },
+  {
+    "revision": "d5d8e8c6a8216600363b",
+    "url": "/family-share/static/js/runtime-main.25996c25.js"
+  }
+]);
